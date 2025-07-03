1ba07ed988f6fbc148dd346a2df1cf89c19934e9
Vahe <ohanyanv703@gmail.com>
2025-07-04 00:22:49 +0400

    full Login logic fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ export const Login = () => {
   const [password, setPassword] = useState(undefined);
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
-  const [state,setState] = useState(false)
+  const [state, setState] = useState(false);
 
   const validateEmail = (email) => {
     const trimmedEmail = email.trim().toLowerCase();
@@ -41,138 +41,140 @@ export const Login = () => {
     setEmailError(emailValid ? "" : "Invalid email address");
     setPasswordError(passwordValid ? "" : "Invalid password length");
 
-    if(email && password) {
-      setState(true)
+    if (email && password) {
+      setState(true);
     }
     console.log("Form Submitted: ");
   };
 
   return (
     <React.Fragment>
-
-  {!state ? (
-    <div className="container">
-        <div className="headerText">
-          <img
-            src="src\assets\around-the-world.png"
-            alt="icon image"
-            width={50}
-            height={50}
-          />
-          <h1>Welcome Back</h1>
-          <p>Please enter your details to sign-in</p>
-        </div>
-
-        <div className="headerBtn">
-          <button>
-            <img src="src/assets/google.png" alt="google logo" />
-          </button>
-          <button>
-            <img src="src/assets/apple.png" alt="apple logo" />
-          </button>
-          <button>
-            <img src="src/assets/facebook.png" alt="facebook logo" />
-          </button>
-        </div>
-
-        <div className="separator">
-          <div className="separator-line"></div>
-          <span className="separator-text">or</span>
-        </div>
-
-        <form onSubmit={handleSubmit}>
-          <div className="email">
-            <label htmlFor="email">Email address</label>
-            <input
-              type="email"
-              placeholder="Enter your Email"
-              onChange={(e) => {
-                const value = e.target.value;
-                setEmail(value);
-                if (validateEmail(value)) {
-                  setEmailError("");
-                } else {
-                  setEmailError("Invalid Email address");
-                }
-              }}
+      {!state ? (
+        <div className="container">
+          <div className="headerText">
+            <img
+              src="src\assets\around-the-world.png"
+              alt="icon image"
+              width={50}
+              height={50}
             />
-            {emailError && <p className="errorText">{emailError}</p>}
+            <h1>Welcome Back</h1>
+            <p>Please enter your details to sign-in</p>
           </div>
-          <div className="password">
-            <label htmlFor="">Password</label>
-            <input
-              type={showPasswd ? "text" : "password"}
-              id="password"
-              placeholder="Enter your Password"
-              onChange={(e) => {
-                const value = e.target.value;
-                setPassword(value);
-                if (validatePassword(value)) {
-                  setPasswordError("");
-                } else {
-                  setPasswordError("Inavlid Password Length");
-                }
-              }}
-              required
-              />
-            {passwordError && <p className="errorText">{passwordError}</p>}
-            <button
-              className="passwordBtn"
-              type="button"
-              onClick={() => {
-                setshowPasswd(!showPasswd);
-              }}
-              >
-              {showPasswd ? (
-                <svg
-                className="eye-icon"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                  <circle cx="12" cy="12" r="3" />
-                </svg>
-              ) : (
-                <svg
-                className="eye-icon"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  >
-                  <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
-                  <line x1="1" y1="1" x2="23" y2="23" />
-                </svg>
-              )}
+
+          <div className="headerBtn">
+            <button>
+              <img src="src/assets/google.png" alt="google logo" />
+            </button>
+            <button>
+              <img src="src/assets/apple.png" alt="apple logo" />
+            </button>
+            <button>
+              <img src="src/assets/facebook.png" alt="facebook logo" />
             </button>
           </div>
-          <div className="forgetPswd">
-            <input type="checkbox" className="checkbox" />
-            <p>Remember for 30 days</p>
-            <a href="/">Forgot password?</a>
-          </div>
+
           <div className="separator">
             <div className="separator-line"></div>
+            <span className="separator-text">or</span>
           </div>
-          <div className="signinPart">
-            <button className="signinBtn" type="submit" disabled={disableBtn()}>
-              Sign in
-            </button>
-            <p>
-              Don't have an account? <a href="">Create account</a>
-            </p>
-          </div>
-        </form>
-      </div>  
-    ) : (
-      <div className="successPage">
-        <h2>Loggin Successful</h2>
-        <p>Welcome back</p>
-      </div>
-    )}
-    </React.Fragment>
-  )
 
+          <form onSubmit={handleSubmit}>
+            <div className="email">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                placeholder="Enter your Email"
+                onChange={(e) => {
+                  const value = e.target.value;
+                  setEmail(value);
+                  if (validateEmail(value)) {
+                    setEmailError("");
+                  } else {
+                    setEmailError("Invalid Email address");
+                  }
+                }}
+              />
+              {emailError && <p className="errorText">{emailError}</p>}
+            </div>
+            <div className="password">
+              <label htmlFor="">Password</label>
+              <input
+                type={showPasswd ? "text" : "password"}
+                id="password"
+                placeholder="Enter your Password"
+                onChange={(e) => {
+                  const value = e.target.value;
+                  setPassword(value);
+                  if (validatePassword(value)) {
+                    setPasswordError("");
+                  } else {
+                    setPasswordError("Inavlid Password Length");
+                  }
+                }}
+                required
+              />
+              {passwordError && <p className="errorText">{passwordError}</p>}
+              <button
+                className="passwordBtn"
+                type="button"
+                onClick={() => {
+                  setshowPasswd(!showPasswd);
+                }}
+              >
+                {showPasswd ? (
+                  <svg
+                    className="eye-icon"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                    <circle cx="12" cy="12" r="3" />
+                  </svg>
+                ) : (
+                  <svg
+                    className="eye-icon"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
+                    <line x1="1" y1="1" x2="23" y2="23" />
+                  </svg>
+                )}
+              </button>
+            </div>
+            <div className="forgetPswd">
+              <input type="checkbox" className="checkbox" />
+              <p>Remember for 30 days</p>
+              <a href="/">Forgot password?</a>
+            </div>
+            <div className="separator">
+              <div className="separator-line"></div>
+            </div>
+            <div className="signinPart">
+              <button
+                className="signinBtn"
+                type="submit"
+                disabled={disableBtn()}
+              >
+                Sign in
+              </button>
+              <p>
+                Don't have an account? <a href="">Create account</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      ) : (
+        <div className="successPage">
+          <h2>Loggin Successful</h2>
+          <p>Welcome back</p>
+        </div>
+      )}
+    </React.Fragment>
+  );
 };
