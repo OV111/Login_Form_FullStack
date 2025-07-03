f9597f8756004a53790afd344edf5b28695d82e8
Vahe <ohanyanv703@gmail.com>
2025-07-03 16:03:17 +0400

    added app jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Login } from "./components/Login";
+import React from "react"
+
+export const App = () => {
+  return (
+    <React.Fragment>
+      <Login/>
+    </React.Fragment>
+  )
+}
+export default App
