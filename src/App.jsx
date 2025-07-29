a7746b58bb4d613e0aa340b80859fbb00395f3a2
Vahe <ohanyanv703@gmail.com>
2025-07-30 01:23:50 +0400

    Routes for /logOut and /DeleteAccount

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login";
 import { SignUp } from "./components/SignUp";
+import { SuccesFullLogOut } from "./components/SuccessfullLogOut";
+import { DeleteAccount } from "./components/DeleteAccount";
 export const App = () => {
   return (
     <React.Fragment>
@@ -9,6 +11,8 @@ export const App = () => {
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/signUp" element={<SignUp />}></Route>
+          <Route path="/logOut" element={<SuccesFullLogOut />}></Route>
+          <Route path="/DeleteAccount" element={<DeleteAccount />}></Route>
         </Routes>
       </BrowserRouter>
     </React.Fragment>
